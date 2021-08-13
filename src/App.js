7577e5f11a7162bc49d4cb7c7bb1c99ff8c5053c
Patrik <patriktao@gmail.com>
2021-08-13 13:15:34 +0200

    Frontend: Ant Design Set Up

--- src/App.js
+++ src/App.js
@@ -1,57 +1,60 @@
-import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
-import "./index.css";
 import { Button } from "antd";
+import About from "./About.js";
+import { Layout, Menu } from "antd";
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+const { Header, Content, Footer, Sider } = Layout;
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-        
-      </div>
+      <Layout>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              Dashboard
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              Inventory
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              Products
+            </Menu.Item>
+            <Menu.Item key="4" icon={<UserOutlined />}>
+              Reports
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="Content">
+          <Header
+            className="site-layout-sub-header-background"
+            style={{ paddingLeft: 24 }}
+          > Header </Header>
+          <Content style={{ margin: "24px 16px 0" }}>
+            Content
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Accelerator Lab @2021
+          </Footer>
+        </Layout>
+      </Layout>
     </Router>
   );
 };
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
-
 export default App;
-
